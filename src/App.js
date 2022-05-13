c2d015ac89d40d1d8d5cdea39cb670646ac1d5d8
kevin-the-engi <kevin.the.engi@gmail.com>
2022-05-13 12:10:17 -0700

    Update App with base api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 import Form from './components/Form/Form.jsx';
 
-function App() {
+const App = () => {
+  const [prompts, addPrompt] = useState([]);
+
+  const getResponse = ({ prompt }) => {
+    const api = "https://api.openai.com/v1/engines/text-curie-001/completions";
+    const options = {
+      prompt, 
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0
+    }
+
+    axios.post(api, options)
+      .then((res) => {
+
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className="App-header">
           Fun with AI
-      </header>
-      <Form/>
+      </h1>
+      <Form getResponse={getResponse} />
     </div>
   );
 }
